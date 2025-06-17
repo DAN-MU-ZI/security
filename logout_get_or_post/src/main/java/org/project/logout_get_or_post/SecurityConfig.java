5c967fad4c6b73b2c22d0f477b58c58c12f41255
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2025-06-17 21:13:43 +0900

    GET 기반 로그아웃 요청 설정

--- logout_get_or_post/src/main/java/org/project/logout_get_or_post/SecurityConfig.java
+++ logout_get_or_post/src/main/java/org/project/logout_get_or_post/SecurityConfig.java
@@ -1,13 +1,12 @@
 package org.project.logout_get_or_post;
 
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -26,6 +25,10 @@ public class SecurityConfig {
 				.anyRequest().authenticated()
 			)
 			.formLogin(AbstractAuthenticationFilterConfigurer::permitAll
+			)
+			.logout(logout->logout
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+				.permitAll()
 			);
 		return http.build();
 	}
